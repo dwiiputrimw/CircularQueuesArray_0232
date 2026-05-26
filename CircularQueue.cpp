198b5cca229d1fda1be1c6227aa05ef4c8883d1c
dwiiputrimw <dwiiputrimahriani@gmail.com>
2026-05-26 11:16:56 +0700

    Menampilkan pilihan pilihan menu

--- CircularQueue.cpp
+++ CircularQueue.cpp
@@ -107,6 +107,7 @@ public:
                 cout << queue_array[FRONT_Position] << "  ";
                 FRONT_Position++;
             }
+
             FRONT_Position = 0;
 
             // iterasi dari awal array hingga REAR
@@ -116,7 +117,27 @@ public:
                 FRONT_Position++;
             }
             cout << endl;
-
         }
     }
-}
+};
+
+int main()
+{
+    Queues q;
+    char ch;
+
+    while (true)
+    {
+        try
+        {
+            cout << "Menu" << endl;
+            cout << "1. Implement insert operation" << endl;
+            cout << "2. Implement Delete Operation" << endl;
+            cout << "3. Display values " << endl;
+            cout << "4. Exit " << endl;
+            cout << "Enter your choice (1-4): ";
+            cin >> ch;
+            cout << endl;
+        }
+    }
+};
